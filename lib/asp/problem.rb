0511a752d7fbf2b64054c5236ed0a92d8f2efe0d
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-05-11 15:38:36 +0200

    created test cases for actual problem encodings

--- lib/asp/problem.rb
+++ lib/asp/problem.rb
@@ -1,9 +1,17 @@
 module Asp
   class Problem
 
+    def initialize(encoding_string)
+    end
+
     # @return [Boolean] if solutions to the problem exist.
     def satisfiable?
       true
+    end
+
+    # @return [Boolean] if problem is unsatisfiable.
+    def unsatisfiable?
+      not satisfiable?
     end
 
     # @return [Array<Asp::Solution>] all solutions to the problem.
--- spec/asp/problem_spec.rb
+++ spec/asp/problem_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
 
 describe Asp::Problem do
+  subject { Asp::Problem.new(string_encoding) }
+
   context "empty set" do
+    let(:string_encoding) { "" }
     it { is_expected.to be_satisfiable }
     its(:solutions) { are_expected.to have(1).item }
+  end
+
+  context "self negation" do
+    let(:string_encoding) { "a:- not a." }
+    it { is_expected.to be_unsatisfiable }
+    its(:solutions) { are_expected.to have(0).item }
+  end
+
+  context "choice rule" do
+    let(:string_encoding) do
+      %{{ a ; b} .
+        :- a, not b.
+        :- b, not a. }
+    end
+    it { is_expected.to be_satisfiable }
+    its(:solutions) { are_expected.to have(2).items }
   end
 end
