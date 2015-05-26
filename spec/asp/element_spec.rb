396edea6623f64f0918731e88ba0bb859dd77bb1
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-05-26 13:16:36 +0200

    refactoring tests

--- spec/asp/element_spec.rb
+++ spec/asp/element_spec.rb
@@ -1,34 +1,38 @@
 require 'spec_helper'
 
 describe Asp::Element do
-  before(:each) do
-    class AnElement
-      include Asp::Element
-      def self.from(string)
-        if (string.include?("a"))
-          new
-        else
-          nil
+  context "output" do
+    before(:each) do
+      class AnElement
+        include Asp::Element
+        def self.from(string)
+          if (string.include?("a"))
+            new
+          else
+            nil
+          end
         end
       end
     end
-  end
 
-  after(:each) do
+    after(:each) do
       Asp::Memory.instance.forget!
-      Object.send(:remove_const, :AnElement)
-  end
+    end
 
-  context "overridden included methods" do
-    it { expect(AnElement.from("a")).not_to be_nil }
-    it { expect(AnElement.from("b")).to be_nil }
-  end
+    context "included methods can be overriden" do
+      it { expect(AnElement.from("a")).not_to be_nil }
+      it { expect(AnElement.from("b")).to be_nil }
+    end
 
-  context "facts only problem" do
-    let(:init_string) { "a." }
-    let(:problem) { Asp::Problem.new(init_string) }
-    let(:element)   { problem.solutions.first.first }
-    it { expect(problem.solutions.first).to be_a(Array) }
-    it { expect(element).to be_kind_of(Asp::Element) }
+    context "parsing problem solutions" do
+      subject { Asp::Problem.new("a.").solutions }
+      it { is_expected.to have(1).item }
+
+      context "first and only solution" do
+        subject { super().first }
+        it { is_expected.to be_a(Array) }
+        its(:first) {is_expected.to be_kind_of(Asp::Element) }
+      end
+    end
   end
 end
