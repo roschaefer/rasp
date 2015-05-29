0ceb5a80593ff8cc55ca192b23e2628dd28c4245
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-05-29 16:19:45 +0200

    green: Asp::Production can handle a class as desired result

--- lib/asp/production.rb
+++ lib/asp/production.rb
@@ -1,7 +1,10 @@
 module Asp
   class Production < Generator
-    def initialize(result, &block)
-      @asp_representation = result.to_s
+    class UnsafeVariablesError < StandardError
+    end
+
+    def initialize(result, attribute_hash={}, &block)
+      @asp_representation = evaluate_result(result, attribute_hash)
       if block_given?
         @asp_representation << " :- "
         @asp_representation << instance_eval(&block)
@@ -9,5 +12,18 @@ module Asp
       @asp_representation << "."
       self
     end
+
+    def evaluate_result(result, attribute_hash)
+      if (result.respond_to? :asp)
+        unsafe_variables = result.asp_attributes - attribute_hash.keys
+        unless (unsafe_variables.empty?)
+          raise UnsafeVariablesError.new(unsafe_variables)
+        end
+        result.asp(attribute_hash)
+      else
+        result.to_s
+      end
+    end
+
   end
 end
--- spec/asp/production_spec.rb
+++ spec/asp/production_spec.rb
@@ -1,26 +1,50 @@
 require 'spec_helper'
 describe Asp::Production  do
-  describe "#make" do
-    context "without block" do
-      subject { Asp::Production.new("anything") }
-      its(:asp_representation) { is_expected.to eq "anything." }
+  context "without block" do
+    subject { Asp::Production.new("anything") }
+    its(:asp_representation) { is_expected.to eq "anything." }
+  end
+
+  context "block containing a string" do
+    subject { Asp::Production.new("something") { "for_a_reason" } }
+    its(:asp_representation) { is_expected.to eq "something :- for_a_reason." }
+  end
+
+  context "nested block" do
+    describe "#no" do
+      subject { Asp::Production.new("love") { no { "war" }} }
+      its(:asp_representation) { is_expected.to eq "love :- not war." }
     end
 
-    context "block containing a string" do
-      subject { Asp::Production.new("something") { "for_a_reason" } }
-      its(:asp_representation) { is_expected.to eq "something :- for_a_reason." }
+    describe "#no #more_than" do
+      subject { Asp::Production.new("tired") { no { more_than(5) { "coffee" }} } }
+      its(:asp_representation) { is_expected.to eq "tired :- not 6 { coffee }." }
+    end
+  end
+
+  context "result is an Asp::Element" do
+    before(:each) do
+      class DesiredResult 
+        include Asp::Element
+      end
+      class DesiredResultWithAttributes
+        include Asp::Element
+        asp_schema :a, :b
+      end
     end
 
-    context "nested block" do
-      describe "#no" do
-        subject { Asp::Production.new("love") { no { "war" }} }
-        its(:asp_representation) { is_expected.to eq "love :- not war." }
-      end
+    describe "no attributes" do
+      subject { Asp::Production.new(DesiredResult) }
+      its(:asp_representation) { is_expected.to eq "desiredresult()." }
+    end
 
-      describe "#no #more_than" do
-        subject { Asp::Production.new("tired") { no { more_than(5) { "coffee" }} } }
-        its(:asp_representation) { is_expected.to eq "tired :- not 6 { coffee }." }
-      end
+    describe "missing attributes" do
+      it { expect{ Asp::Production.new(DesiredResultWithAttributes) }.to raise_error(Asp::Production::UnsafeVariablesError, /\[:a, :b\]/) }
+    end
+
+    describe "bound attributes" do
+      subject { Asp::Production.new(DesiredResultWithAttributes, :a => "foo", :b => "bar") }
+      its(:asp_representation) { is_expected.to eq "desiredresultwithattributes(foo,bar)." }
     end
   end
 end
