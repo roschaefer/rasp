fcee68a62db301978f7ea97a7ccd46c26ba2148d
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-05-29 15:32:42 +0200

    refactor: Asp::Constraint inherits from Asp::Generator

--- lib/asp/constraint.rb
+++ lib/asp/constraint.rb
@@ -1,26 +1,11 @@
-require 'asp/constraint/same'
 module Asp
-  class Constraint
-    attr_reader :asp_representation
+  class Constraint < Generator
 
     def initialize(&block)
       @asp_representation = ":- "
       @asp_representation << instance_eval(&block)
       @asp_representation << "."
       self
-    end
-
-    def more_than(cardinality, &block)
-      "#{cardinality+1} { " + instance_eval(&block) + " }"
-    end
-
-    def less_than(cardinality, &block)
-      "{ " + instance_eval(&block) + " } #{cardinality-1}"
-    end
-
-    def conjunct(&block)
-      array = instance_eval(&block)
-      array.join(", ")
     end
 
     def same(*attributes)
