d4c24beef8c228868b75f63b882a59026a7bc04a
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-06-24 11:32:45 +0200

    implemented name for soft constraint

--- lib/asp/soft_constraint.rb
+++ lib/asp/soft_constraint.rb
@@ -1,9 +1,10 @@
 module Asp
   class SoftConstraint < Generator
-    def initialize(cost, &block)
-      @asp_representation = "#const costs_of_penalty = #{cost}."
+    def initialize(cost, opts={}, &block)
+      opts[:name] ||= "penalty"
+      @asp_representation = "#const costs_of_#{opts[:name]} = #{cost}."
       @asp_representation << "\n"
-      @asp_representation << "penalty(\"penalty\", costs_of_penalty) :- "
+      @asp_representation << "penalty(\"#{opts[:name]}\", costs_of_#{opts[:name]}) :- "
       @asp_representation << instance_eval(&block)
       @asp_representation << "."
       self
--- spec/asp/soft_constraint_spec.rb
+++ spec/asp/soft_constraint_spec.rb
@@ -7,6 +7,11 @@ describe Asp::Constraint do
       expect(constraint.asp_representation).to include 'penalty("penalty", costs_of_penalty) :- a.'
     end
 
+    it "assigns a name for the penalty" do
+      constraint = Asp::SoftConstraint.new(10, :name => "awful_penalty") { "a" }
+      expect(constraint.asp_representation).to include 'penalty("awful_penalty", costs_of_awful_penalty) :- a.'
+    end
+
     it "sets a constant for the penalty cost" do
       constraint = Asp::SoftConstraint.new(10) { "a" }
       expect(constraint.asp_representation).to include "#const costs_of_penalty = 10."
