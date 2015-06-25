a0fb83cd3246c7db2b064688091e64418561b856
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-06-25 19:07:43 +0200

    green: specs for problem with soft constraints and attribute classes

--- spec/asp/integration/advice_spec.rb
+++ spec/asp/integration/advice_spec.rb
@@ -24,24 +24,47 @@ describe "integration test" do
                    """) }
 
 
-    it "problem has got 16 possible solutions" do
-      expect(problem.solutions).to have(16).items
-    end
+    describe "#avoid" do
+      it "problem has got 16 possible solutions" do
+        expect(problem.solutions).to have(16).items
+      end
 
-    it "it's better to have a copy of your passport" do
-      problem.avoid(5) { no { "copy(passport)" } }
-      expect(problem.solutions).to have(8).items
-    end
+      it "predicate with given value" do
+        problem.avoid(5) { no { "copy(passport)" } }
+        expect(problem.solutions).to have(8).items
+      end
 
-    it "hopefully you have got all your shots" do
-      problem.avoid(5) { less_than(2) { "vaccination(_)" } }
-      expect(problem.solutions).to have(4).items
-    end
+      it "predicate with any value" do
+        problem.avoid(5) { less_than(2) { "vaccination(_)" } }
+        expect(problem.solutions).to have(4).items
+      end
 
-    it "the best is to have all your shots and enough copies of your documents" do
-      problem.avoid(5) { less_than(2) { "vaccination(_)" } }
-      problem.avoid(3) { less_than(2) { "copy(_)" } }
-      expect(problem.solutions).to have(1).item
+      it "more than one predicate" do
+        problem.avoid(5) { less_than(2) { "vaccination(_)" } }
+        problem.avoid(3) { less_than(2) { "copy(_)" } }
+        expect(problem.solutions).to have(1).item
+      end
+
+      context "for attribute classes" do
+        before(:each) do
+          class Copy
+            include Asp::Element
+            asp_schema :document
+          end
+        end
+
+        it "with any value" do
+          problem.avoid(2) { less_than(2) { Copy.asp() } }
+          expect(problem.solutions).to have(4).items 
+        end
+
+        it "with fixed value" do
+          problem.avoid(2) { no { Copy.asp(:document => "passport") } }
+          expect(problem.solutions).to have(8).items 
+        end
+
+      end
+
     end
   end
 end
