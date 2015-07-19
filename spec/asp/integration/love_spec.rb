513bf75df5bc44749b5d16b90fadaccfdf1993c4
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-07-19 23:40:25 +0200

    refactored love spec

--- spec/asp/integration/love_spec.rb
+++ spec/asp/integration/love_spec.rb
@@ -4,9 +4,6 @@ describe "dating agency" do
     class Love
       include Asp::Element
       asp_schema :name
-      def self.match?(string)
-        string.include?("love")
-      end
     end
 
     class Person
@@ -14,6 +11,15 @@ describe "dating agency" do
       def self.match?(string)
         nil
       end
+      def initialize(name, sex, status)
+        @name = name || "_"
+        @sex = sex || "_"
+        @status = status || "_"
+      end
+      def asp_representation
+        "person(\"#{@name}\", #{@sex}, #{@status})"
+      end
+
       asp_schema :name, :sex, :relationship_status
     end
   end
@@ -21,30 +27,45 @@ describe "dating agency" do
   after(:all) do
     Asp::Memory.instance.forget!
   end
-  context "restrictions" do
-    let(:problem) { Asp::Problem.new(
-      %{person("Alice", female, single).
-         person("Bob", male, single).
-         person("Carla", female, relationship).
-         { love(NAME) } 1 :- person(NAME,_,_).}
-    ) }
 
-    it "we want them all" do
+
+  context "online dating site" do
+    subject (:problem) {
+        Asp::Problem.new().tap do |p|
+          p.add(Person.new("Alice", :female, :single))
+          p.add(Person.new("Bob",   :male,   :single))
+          p.add(Person.new("Carla", :female, :married))
+          p.add("{ love(NAME) } 1 :- person(NAME,_,_)")
+        end
+    }
+
+    before(:each) do
       problem.never { less_than(1) { Love.asp } }
       problem.never { more_than(1) { Love.asp } }
-      expect(problem.solutions).to correspond_with [ ['love("Alice")'], ['love("Bob")'], ['love("Carla")']]
     end
 
-    it "only solo females" do
-      problem.never { less_than(1) { Love.asp } }
-      problem.never { conjunct{[
-          Love.asp(:name => "NAME"),
-          Person.asp(:name => "NAME", :sex => "male")]
-      } }
-      problem.never { conjunct{[
-          Love.asp(:name => "NAME"),
-          Person.asp(:name => "NAME", :relationship_status => "relationship")]
-      } }
+    let(:solo_constraint) do
+      problem.never { conjunct{ [Love.asp(:name => "NAME"), Person.asp(:name => "NAME", :relationship_status => "married")] } }
+    end
+
+    let(:female_constraint) do
+      problem.never { conjunct{[ Love.asp(:name => "NAME"), Person.asp(:name => "NAME", :sex => "male")] } }
+    end
+
+    its(:solutions) { are_expected.to correspond_with [['love("Alice")'], ['love("Bob")'], ['love("Carla")']] }
+
+    it "only females" do
+      female_constraint
+      expect(problem.solutions).to correspond_with [['love("Alice")'], ['love("Carla")']]
+    end
+
+    it "only singles" do
+      solo_constraint
+      expect(problem.solutions).to correspond_with [['love("Alice")'], ['love("Bob")']]
+    end
+
+    it "only female singles" do
+      solo_constraint; female_constraint
       expect(problem.solutions).to correspond_with [['love("Alice")']]
     end
   end
